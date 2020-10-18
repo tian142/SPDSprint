95b2744feeddda6a2193a657e922e94854083656
Tian Qiu <tian.qiu142@gmail.com>
2020-10-17 22:33:50 -0700

    upgraded Modal

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import GameDisplay from './components/GameDisplay';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import TopNav from './components/TopNav';
-import CreateLobbyPopUp from './components/CreateLobbyPopUp';
 
-const useStyles = makeStyles({
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
   topZ: {
     zIndex: 1000,
   },
@@ -29,13 +32,24 @@ const useStyles = makeStyles({
   bgRed: {
     background: 'pink',
   },
-});
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const App = () => {
   const classes = useStyles();
   const [lobbies, setLobbies] = useState([]);
   const [currentLobby, setCurrentLobby] = useState('');
-  const [lobbyCreating, setLobbyCreating] = useState(false);
+  const [lobbyCreating, setLobbyCreating] = useState(true);
 
   return (
     <>
@@ -52,8 +66,8 @@ const App = () => {
           className={classes.centerGrid}
           container
           direction="column"
-          justify="flex-start"
-          alignItems="center"
+          // justify="flex-start"
+          // alignItems="center"
         >
           <SideNav />
           <Grid item>
@@ -62,19 +76,37 @@ const App = () => {
           <Lobbies lobbies={lobbies} />
         </Grid>
       </Grid>
-      <CreateLobbyPopUp
+      {/* <CreateLobbyPopUp
         className={classes.topGrid}
         lobbyCreating={lobbyCreating}
         onClose={() => setLobbyCreating(false)}
       >
-        <CreateLobby
-          className={classes.topGrid}
-          lobbies={lobbies}
-          setLobbies={setLobbies}
-          currentLobby={currentLobby}
-          setCurrentLobby={setCurrentLobby}
-        />
-      </CreateLobbyPopUp>
+
+      </CreateLobbyPopUp> */}
+      <Modal
+        // aria-labelledby="transition-modal-title"
+        // aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={lobbyCreating}
+        onClose={() => setLobbyCreating(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={lobbyCreating}>
+          <div className={classes.paper}>
+            <CreateLobby
+              className={classes.topGrid}
+              lobbies={lobbies}
+              setLobbies={setLobbies}
+              currentLobby={currentLobby}
+              setCurrentLobby={setCurrentLobby}
+            />
+          </div>
+        </Fade>
+      </Modal>
     </>
   );
 };
--- src/components/CreateLobby.js
+++ src/components/CreateLobby.js
@@ -2,11 +2,32 @@ import React, { useEffect } from 'react';
 import { Formik, useField, Form } from 'formik';
 import * as Yup from 'yup';
 import { makeStyles } from '@material-ui/core/styles';
-// import classes from '*.module.css';
+import CreateLobbyGame from './CreateLobbyGame';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles({
   createLobbyForm: {
     background: 'pink',
+  },
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
   },
 });
 
@@ -72,40 +93,14 @@ const CreateLobby = ({
           mic: '',
         }}
         validationSchema={Yup.object({
-          game: Yup.string()
-            .oneOf(
-              [
-                'League of Legends',
-                'CS:GO',
-                'Escape From Tarkov',
-                'Among Us',
-                'Valorant',
-                'other',
-              ],
-              'Invalid Game'
-            )
-            .required('Required'),
+          game: Yup.string().required('Required'),
           lobbySize: Yup.number()
             .required()
             .positive('Lobby size must be at least 2')
             .integer()
             .min(2, 'Lobby size must be at least 2')
             .max(15, 'Max lobby size is 15'),
-          language: Yup.string()
-            .oneOf(
-              [
-                'English',
-                'Chinese',
-                'Spanish',
-                'Portuguese',
-                'Russian',
-                'Japanese',
-                'Turkish',
-                'Other',
-              ],
-              'Please Select Language'
-            )
-            .required('Required'),
+          language: Yup.string().required('Required'),
           mic: Yup.string()
             .oneOf(
               ['Mandatory', 'Perfered', 'No Mic'],
@@ -124,53 +119,59 @@ const CreateLobby = ({
           console.log(lobbies);
         }}
       >
-        {(props) => (
-          <Form>
-            <div>Create a Lobby</div>
-            <CustomSelect label="game:" name="game">
-              <option value="">Select a Game</option>
-              <option value="League of Legends">League of Legends</option>
-              <option value="CS:GO">CS:GO</option>
-              <option value="Escape From Tarkov">Escape From Tarkov</option>
-              <option value="Among Us">Among Us</option>
-              <option value="Valorant">Valorant</option>
-              <option value="other">other</option>
-            </CustomSelect>
-            <CustomNumInput
-              label="Size"
-              name="lobbySize"
-              type="number"
-              placeholder="1"
-            />
-            <CustomSelect label="Language" name="language">
-              <option value="">Lobby Language</option>
-              <option value="English">English</option>
-              <option value="Spanish">Spanish</option>
-              <option value="Portuguese">Portuguese</option>
-              <option value="Russian">Russian</option>
-              <option value="Turkish">Turkish</option>
-              <option value="Japanese">Japanese</option>
-              <option value="Chinese">Chinese</option>
-              <option value="Other">Other</option>
-            </CustomSelect>
-            <CustomSelect label="Mic" name="mic">
-              <option value="">Mic Preference</option>
-              <option value="Mandatory">Mandatory</option>
-              <option value="Perfered">Perfered</option>
-              <option value="No Mic">No Mic</option>
-            </CustomSelect>
-            <CustomTextInput
-              label="Lobby Notes"
-              name="lobbyNotes"
-              type="text"
-              placeholder="(Max 140 Characters)"
-            />
-
-            <button type="submit">
-              {props.isSubmitting ? 'Loading...' : 'Submit'}
-            </button>
-          </Form>
-        )}
+        <Form>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2" align="center">
+                Create a Lobby
+              </Typography>
+              <Divider variant="middle" />
+              <CreateLobbyGame />
+              <CustomSelect label="game:" name="game">
+                <option value="">Select a Game</option>
+                <option value="League of Legends">League of Legends</option>
+                <option value="CS:GO">CS:GO</option>
+                <option value="Escape From Tarkov">Escape From Tarkov</option>
+                <option value="Among Us">Among Us</option>
+                <option value="Valorant">Valorant</option>
+                <option value="other">other</option>
+              </CustomSelect>
+              <CustomNumInput
+                label="Size"
+                name="lobbySize"
+                type="number"
+                placeholder="1"
+              />
+              <CustomSelect label="Language" name="language">
+                <option value="">Lobby Language</option>
+                <option value="English">English</option>
+                <option value="Spanish">Spanish</option>
+                <option value="Portuguese">Portuguese</option>
+                <option value="Russian">Russian</option>
+                <option value="Turkish">Turkish</option>
+                <option value="Japanese">Japanese</option>
+                <option value="Chinese">Chinese</option>
+                <option value="Other">Other</option>
+              </CustomSelect>
+              <CustomSelect label="Mic" name="mic">
+                <option value="">Mic Preference</option>
+                <option value="Mandatory">Mandatory</option>
+                <option value="Perfered">Perfered</option>
+                <option value="No Mic">No Mic</option>
+              </CustomSelect>
+              <CustomTextInput
+                label="Lobby Notes"
+                name="lobbyNotes"
+                type="text"
+                placeholder="(Max 140 Characters)"
+              />
+            </CardContent>
+            <CardActions>
+              <button type="submit">Submit</button>
+              {/* <Button size="small">Create</Button> */}
+            </CardActions>
+          </Card>
+        </Form>
       </Formik>
     </div>
   );
